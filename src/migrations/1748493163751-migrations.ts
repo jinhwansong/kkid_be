f08c54de2a8b29bf2d88301bbf54601d82863381
jinhwan <song7022556@gmail.com>
2025-06-13 10:34:25 +0900

    fix:마이그레이션수정

--- src/migrations/1748493163751-migrations.ts
+++ src/migrations/1748493163751-migrations.ts
@@ -4,8 +4,11 @@ export class Migrations1748493163751 implements MigrationInterface {
     name = 'Migrations1748493163751'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`user\` ADD \`username\` varchar(30) NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`user\` ADD UNIQUE INDEX \`IDX_78a916df40e02a9deb1c4b75ed\` (\`username\`)`);
+        // 컬럼 구조를 수정할 경우
+  await queryRunner.query(`ALTER TABLE \`user\` MODIFY COLUMN \`username\` varchar(30) NOT NULL`);
+
+  // unique 인덱스가 없다면 추가 (MySQL은 중복 시 무시 안 함 주의)
+  await queryRunner.query(`CREATE UNIQUE INDEX \`IDX_78a916df40e02a9deb1c4b75ed\` ON \`user\` (\`username\`)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
